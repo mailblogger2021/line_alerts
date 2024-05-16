aa38145d4ae42f87459a48fa709ef51469df664d
Logesh Chandran <60649813+Logeshkc77@users.noreply.github.com>
2024-05-16 18:56:13 +0530

    yaml file edited

--- yaml_file_editing.py
+++ yaml_file_editing.py
@@ -1,14 +1,20 @@
 import datetime
 import yaml
-
+import os
 def yaml_file_edit(minutes,file_name):
     current_time = datetime.datetime.now()
     cron_time = current_time + datetime.timedelta(minutes=minutes)
 
     cron_value = f"{cron_time.minute} {cron_time.hour} {cron_time.day} {cron_time.month} *"
 
-
-    yaml_file_path = f'.github\\workflows\\{file_name}.yml'
+    current_directory = os.getcwd()
+    all_directories = []
+    for root, dirs, files in os.walk(current_directory):
+        all_directories.extend([os.path.join(root, d) for d in dirs])
+    print("All directories in the current directory and its subdirectories:")
+    for directory in all_directories:
+        print(directory)
+    yaml_file_path = f'.github/workflows/{file_name}.yml'
     with open(yaml_file_path, 'r') as file:
         yaml_data = yaml.safe_load(file)
 
